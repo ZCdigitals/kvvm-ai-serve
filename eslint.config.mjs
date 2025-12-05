ce781e5f0ae8c3f202cac40defa2dad09e116a94
alex <liuhlightning@qq.com>
2025-12-05 11:26:53 +0800

    upgrade deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,25 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    // 0 off 1 warn 2 error
-    rules: {
-      "@typescript-eslint/ban-ts-comment": 0,
-      "@typescript-eslint/no-unused-vars": 0,
-      quotes: [2, "double"],
-      semi: [2, "always"],
-      "no-console": 0,
+export default defineConfig({
+  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  files: ["**/*.{ts}"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: globals.node,
+    parserOptions: {
+      parser: tseslint.parser,
     },
-    ignores: ["node_modules", "dist"],
   },
-);
+  // 0 off 1 warn 2 error
+  rules: {
+    "@typescript-eslint/ban-ts-comment": 0,
+    quotes: [2, "double"],
+    semi: [2, "always"],
+    "no-console": 0,
+  },
+  ignores: ["node_modules/*", "dist/*", "build"],
+});
